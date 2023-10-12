a32e240c599df565ba4128c2cd3f0d9f54345fef
Costin Chelu <costin.chelu@verifone.com>
2023-10-12 11:53:31 +0300

    Changes in TwoSum Algorithm

--- src/main/java/edu/princeton/cs/algs4/section_1_4/TwoSumFast.java
+++ src/main/java/edu/princeton/cs/algs4/section_1_4/TwoSumFast.java
@@ -8,7 +8,20 @@ import edu.princeton.cs.algs4.section_1_1.BinarySearch;
 
 public class TwoSumFast {
 
-   /** Count pairs that sum to 0. */
+
+   /**
+    * Count pairs that sum to 0.
+    * <p></p>
+    * Uses a merge sort and a binary search to create a logarithmic solution for the problem
+    * <p></p>
+    * because only an integer and the inverse of that integer can be summed up to 0, we will search for each of the
+    * numbers, their reversed value in the array.
+    * <p></p>
+    * case nothing is found binary search will return -1 (no count)
+    * <p></p>
+    * case the returned index is lower than the index we are searching, we will not count because
+    * we don't want double counting
+    * */
    public static int count(int[] a) {
       Arrays.sort(a);
       int N = a.length;
@@ -22,7 +35,7 @@ public class TwoSumFast {
    public static void main(String[] args) throws Exception {
       In getFileName = new In();
       In in = new In(getFileName.getFileFromResources("8Kints.txt"));
-      int[] a = Arrays.stream(in.readAllLines()).mapToInt(Integer::valueOf).toArray();
+      int[] a = Arrays.stream(in.readAllLines()).map(String::trim).mapToInt(Integer::valueOf).toArray();
       Stopwatch stopwatch = new Stopwatch();
       StdOut.println(count(a));
       StdOut.printf("%e (%.2f seconds)\n", 0.0, stopwatch.elapsedTime());
